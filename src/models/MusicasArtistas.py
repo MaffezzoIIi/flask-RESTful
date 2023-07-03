b0d045c918cc66da1cce086057f73feb6d33aea7
Thomas <thomasmaffezzolli8@gmail.com>
2023-07-03 20:43:51 -0300

    Crud da model musicas artistas

--- src/models/MusicasArtistas.py
+++ src/models/MusicasArtistas.py
@@ -1,3 +1,7 @@
+from src.db.banco import Banco
+
+mydb = Banco()
+
 class MusicasArtistas():
 
     def __init__(self, musica_id, artista_id):
@@ -15,3 +19,58 @@ class MusicasArtistas():
 
     def setArtista_id(self, artista_id):
         self.artista_id = artista_id
+
+    def save(musicas_artistas):
+        cursor = mydb.getCursor()
+
+        sql = "INSERT INTO musicas_artistas (musica_id, artista_id) VALUES (%s, %s)"
+        val = (musicas_artistas.getMusica_id(), musicas_artistas.getArtista_id())
+
+        cursor.execute(sql, val)
+        mydb.commit()
+
+        return musicas_artistas
+    
+    def removeByMusicas(musica_id):
+        cursor = mydb.getCursor()
+
+        sql = "DELETE FROM musicas_artistas WHERE musica_id = %s"
+        val = (musica_id,)
+
+        cursor.execute(sql, val)
+        mydb.commit()
+
+        return True
+    
+    def removeByArtistas(artista_id):
+        cursor = mydb.getCursor()
+
+        sql = "DELETE FROM musicas_artistas WHERE artista_id = %s"
+        val = (artista_id,)
+
+        cursor.execute(sql, val)
+        mydb.commit()
+
+        return True
+    
+    def getArtistasByMusica(musica_id):
+        cursor = mydb.getCursor()
+
+        sql = "SELECT * FROM musicas_artistas WHERE musica_id = %s"
+        val = (musica_id,)
+
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+
+        return result
+    
+    def getMusicasByArtista(artista_id):
+        cursor = mydb.getCursor()
+
+        sql = "SELECT * FROM musicas_artistas WHERE artista_id = %s"
+        val = (artista_id,)
+
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+
+        return result
